71058a56c676429e5ba0384b94e00c3b25a15ad9
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-02-19 10:22:05 +0100

    added help section

--- tempsms.py
+++ tempsms.py
@@ -5,13 +5,18 @@ global pattern, timeRange
 pattern = ''
 timeRange = 120
 
+def usage():
+  print '\nUsage: \n'
+  print 'python '+sys.argv[0]+' --country=usa|canada|united-kingdom fetches a temporary number for specified country'
+  print 'python '+sys.argv[0]+' --check=243840389473 watches the given number for incoming sms\n\n'
+
 def main():
     try:                                
-        opts, args = getopt.getopt(sys.argv[1:], "cC", ["country=", "check="])
-    except getopt.GetoptError:   
-        print "oops"       
-        usage()                         
-        sys.exit(2)                     
+        opts, args = getopt.getopt(sys.argv[1:], "hcC", ["help", "country=", "check="])
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+        sys.exit(2)                  
     for opt, arg in opts:
         m = smsreceivefree.generator()
         if opt in ("-c", "--country"):
@@ -22,19 +27,26 @@ def main():
             number, countryCode = m.getNumber(m.country)
             print countryCode, number
         elif opt in ("-C", "--check"):
-            m.number = arg
-            m.path = "info/{0}".format(m.number)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            try:
-              while True:
-                now = datetime.datetime.now()
-                print "Messages to {0} @ {1}\n".format(m.number, now.strftime("%Y-%m-%d %H:%M"))
-                print m.checkSMS(pattern)
-                print("\n")
-                time.sleep(timeRange)
-            except KeyboardInterrupt:
-              print('\n\nKeyboard exception received. Exiting.')
-              exit()
+            if not arg:
+                print "no number given"
+                exit()
+            else:
+                m.number = arg
+                m.path = "info/{0}".format(m.number)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                try:
+                    while True:
+                        now = datetime.datetime.now()
+                        print "Messages to {0} @ {1}\n".format(m.number, now.strftime("%Y-%m-%d %H:%M"))
+                        print m.checkSMS(pattern)
+                        print("\n")
+                        time.sleep(timeRange)
+                except KeyboardInterrupt:
+                    print('\n\nKeyboard exception received. Exiting.')
+                    exit()
+        elif opt in ("-h", "--help"):
+            usage()
+            exit()
 
 if __name__ == "__main__":
   main()
