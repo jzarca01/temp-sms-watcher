24f910d36011ace168d58c739288d62456d10c38
Rushi Chaudhari <rushic24@gmail.com>
2020-04-21 19:46:04 -0400

    fixed for python3 and for new site

--- tempsms.py
+++ tempsms.py
@@ -3,18 +3,18 @@ from numberGen import smsreceivefree
 
 global pattern, timeRange
 pattern = ''
-timeRange = 120
+timeRange = 20
 
 def usage():
-  print '\nUsage: \n'
-  print 'python '+sys.argv[0]+' --country=usa|canada|united-kingdom fetches a temporary number for specified country'
-  print 'python '+sys.argv[0]+' --check=243840389473 watches the given number for incoming sms\n\n'
+  print ('\nUsage: \n')
+  print ('python '+sys.argv[0]+' --country=usa|canada|united-kingdom fetches a temporary number for specified country')
+  print ('python '+sys.argv[0]+' --check=243840389473 watches the given number for incoming sms\n\n')
 
 def main():
     try:                                
         opts, args = getopt.getopt(sys.argv[1:], "hcC", ["help", "country=", "check="])
     except getopt.GetoptError as err:
-        print str(err)
+        print (str(err))
         usage()
         sys.exit(2)                  
     for opt, arg in opts:
@@ -23,12 +23,12 @@ def main():
             m.country = arg
             if not arg:
                 m.country = "united-kingdom"
-                print "united-kingdom set as default, other options: are usa|canada|united-kingdom"
+                print ("united-kingdom set as default, other options: are usa|canada|united-kingdom")
             number, countryCode = m.getNumber(m.country)
-            print countryCode, number
+            print (countryCode, number)
         elif opt in ("-C", "--check"):
             if not arg:
-                print "no number given"
+                print ("no number given")
                 exit()
             else:
                 m.number = arg
@@ -37,9 +37,9 @@ def main():
                 try:
                     while True:
                         now = datetime.datetime.now()
-                        print "Messages to {0} @ {1}\n".format(m.number, now.strftime("%Y-%m-%d %H:%M"))
-                        print m.checkSMS(pattern)
-                        print("\n")
+                        print("Messages to {0} @ {1}\n".format(m.number, now.strftime("%Y-%m-%d %H:%M")))
+                        print(m.checkSMS(pattern))
+                        print(("\n"))
                         time.sleep(timeRange)
                 except KeyboardInterrupt:
                     print('\n\nKeyboard exception received. Exiting.')
